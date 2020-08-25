151eed08c01df8836111fb17fd35161f9ad740e8
Суюн Сарбаев <s.sarbayev@corp.p-s.kz>
2020-08-25 12:18:08 +0600

    Okay third

--- app/src/main/java/suyun/example/training/MainActivity.kt
+++ app/src/main/java/suyun/example/training/MainActivity.kt
@@ -9,6 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("First", "3")
-        Log.d("Second", "line")
     }
 }
