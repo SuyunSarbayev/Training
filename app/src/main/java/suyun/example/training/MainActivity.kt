63099d4c8c430c855361fbfe725f41027e792dfc
Суюн Сарбаев <s.sarbayev@corp.p-s.kz>
2020-08-25 12:06:19 +0600

    First

--- app/src/main/java/suyun/example/training/MainActivity.kt
+++ app/src/main/java/suyun/example/training/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("First", "Oops forgot something for SECOND Feature")
+        Log.d("First", "First")
     }
 }
