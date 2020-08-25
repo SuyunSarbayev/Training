f35c399149339bd4c3771b7c223d2b281ea3c100
Суюн Сарбаев <s.sarbayev@corp.p-s.kz>
2020-08-25 12:05:20 +0600

    Oops forgot something for SECOND feature

--- app/src/main/java/suyun/example/training/MainActivity.kt
+++ app/src/main/java/suyun/example/training/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("First", "Third Feature")
+        Log.d("First", "Oops forgot something for SECOND Feature")
     }
 }
