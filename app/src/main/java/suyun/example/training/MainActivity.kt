2f34484af3c288d8e29f9d11627b58a0bd663298
Суюн Сарбаев <s.sarbayev@corp.p-s.kz>
2020-08-25 12:17:54 +0600

    Okay second

--- app/src/main/java/suyun/example/training/MainActivity.kt
+++ app/src/main/java/suyun/example/training/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("First", "1")
+        Log.d("First", "3")
+        Log.d("Second", "line")
     }
 }
